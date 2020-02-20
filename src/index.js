831f8e08a4db61496479f0c12a0ca1bf8362e473
kagafon <k.agafonov@ase-ec.ru>
2020-02-20 13:44:19 +0300

    init: min, max, avg functions implementation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
+  if (array && array.length > 0){
+    return array.reduce((min, current) => min = min > current ? current : min, Infinity);
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (array && array.length > 0){
+    return array.reduce((max, current) => max = max < current ? current : max, -Infinity);
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (array && array.length > 0){
+    return array.reduce((sum, current) => sum += current, 0)/array.length;
+  } 
   return 0;
 }
